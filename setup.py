f98d94d7442ac2372026d6ccea979ac25bb5c1db
javisantana <jsantana@wtelecom.es>
2011-03-18 16:57:45 +0100

    added setup to include template

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from distutils.core import setup
+import os
+from os.path import join
+
+
+def get_data_files(path):
+    total_files = []
+    for root, dirs, files in os.walk(path):
+        total_files += [join(root, name).split('/',1)[1] for name in files]
+    return total_files
+
 
 setup(name='wtdeploy', 
       author = 'Javi Santana', 
       author_email = 'jsantana@wtelecom.es',
       description = 'django projects deploy on ubuntu',
+      url='https://github.com/wellnesstelecom/wtdeploy',
       version='0.2',
-      packages=['wtdeploy'],
-      requires = ['fabric']
+      packages=['wtdeploy', 'wtdeploy.modules'],
+      requires = ['fabric'],
+      package_data={'wtdeploy': get_data_files('wtdeploy/base_template')}
 )
