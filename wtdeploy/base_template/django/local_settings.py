f55154a0fbf67e85d6726830ff98c3643d5200bd
javisantana <jsantana@wtelecom.es>
2011-03-22 10:30:00 +0100

    upgraded local_settings to django1.2

--- wtdeploy/base_template/django/local_settings.py
+++ wtdeploy/base_template/django/local_settings.py
@@ -4,22 +4,26 @@ LOCAL_DEV = True
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-#sorl-thumbnail
-THUMBNAIL_DEBUG = True
+ADMINS = (¬
+    # ('Your Name', 'your_email@domain.com'),¬
+)¬
 
-#django-contact-form
-DEFAULT_FROM_EMAIL = 'contactform@foo'
+¬
+MANAGERS = ADMINS¬
+¬
+CACHE_BACKEND = 'dummy://'¬
+¬
+DATABASES = {¬
+    'default': {¬
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.¬
+        'NAME': '%(database_name)s',                      # Or path to database file if using sqlite3.¬
+        'USER': '%(database_user)s',                      # Not used with sqlite3.¬
+        'PASSWORD': '%(database_pass)s',                  # Not used with sqlite3.¬
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.¬
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.¬
+    }¬
+}¬
 
-MANAGERS = (
-    ('fooper','fooper@foo'),
-)
-
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '%(database_name)s'             # Or path to database file if using sqlite3.
-DATABASE_USER = '%(database_user)s'             # Not used with sqlite3.
-DATABASE_PASSWORD = '%(database_pass)s'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 try:
    from mobile_local_settings import *
